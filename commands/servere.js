b928eab6c45992c128a23c25113516578cd93ca9
C0oL3r <35148210+C0oL3r@users.noreply.github.com>
2018-01-08 21:55:47 +0200

    Update servere.js

--- commands/servere.js
+++ commands/servere.js
@@ -4,8 +4,8 @@ const request = require('request');
 exports.run = (client, msg, args) => {
 	msg.delete();
 	request('https://api.uptimerobot.com/getMonitors?apiKey=u521977-0499e7f37766d8e20b6f751b&format=json', { json: true }, (err, res, body) => {
-		var json = body.slice(19); var json2 = json.substring(0, 576 - 2);
-		console.log(json2);
+		var json = body.slice(19); var json2 = json.substring(0, 576 - 1);
+		//console.log(json2);
 		var obj = JSON.parse(json2);
 		var onof1;
 		var onof2;
